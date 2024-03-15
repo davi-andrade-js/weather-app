17a5c97b9b32a96769f8783b67d9de435ca3d740
Davi <dv.andrade04@gmail.com>
2024-03-14 23:47:57 -0300

    added first temp container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
 
 interface Weather {
   id: number;
@@ -58,9 +65,84 @@ type WeatherResponse = {
 };
 
 export default function Home() {
+  const { isPending, error, data } = useQuery<WeatherResponse>({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      );
+      return data;
+    },
+    // fetch(
+    //   "https://api.openweathermap.org/data/2.5/forecast?q=brasil&appid=b94313ed55c5f333932023fa6ebdb431&cnt=56"
+    // ).then((res) => res.json()),
+  });
+
+  const firstData = data?.list[0];
+
+  console.log("data", data);
+
+  if (isPending)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    );
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        <section className="space-y-4">
+          <div className="space-y-4">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})</p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl flex">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 296.37)}
+                  <span className="text-2xl">°C</span>
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}
+                    <span>°C</span>
+                  </span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}
+                    <span>°C ↓</span>
+                  </span>
+                  <span>
+                    {convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}
+                    <span>°C ↑</span>
+                  </span>
+                </p>
+              </div>
+              <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
+                {data?.list.map((d, i) => (
+                  <div
+                    key={i}
+                    className="flex flex-col judtify-between gap-2 items-center text0-xs font-semibold"
+                  >
+                    <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), "h:mm a")}</p>
+                    <p className="flex">
+                      {convertKelvinToCelsius(d.main.temp ?? 0)}
+                      <span className="text-1xl">°C</span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </Container>
+          </div>
+        </section>
+
+        <section></section>
+      </main>
     </div>
   );
 }
