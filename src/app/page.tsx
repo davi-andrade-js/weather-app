4f4cbac616abeebc58694b2d4460340b26ca735d
Davi <dv.andrade04@gmail.com>
2024-04-15 20:32:26 -0300

    add weather details container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+
 import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
 import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "react-query";
 import axios from "axios";
-import { format, parseISO } from "date-fns";
+import { format, fromUnixTime, parseISO } from "date-fns";
+import WeatherIcon from "@/components/WeatherIcon";
+import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
+import WeatherDetails from "@/components/WeatherDetails";
+import { metersToKilometers } from "@/utils/metersToKilometers";
+import { convertWindSpeed } from "@/utils/convertWindSpeed";
 
 interface Weather {
   id: number;
@@ -65,7 +71,7 @@ type WeatherResponse = {
 };
 
 export default function Home() {
-  const { isPending, error, data } = useQuery<WeatherResponse>({
+  const {isLoading, error, data } = useQuery<WeatherResponse>({
     queryKey: ["repoData"],
     queryFn: async () => {
       const { data } = await axios.get(
@@ -73,16 +79,13 @@ export default function Home() {
       );
       return data;
     },
-    // fetch(
-    //   "https://api.openweathermap.org/data/2.5/forecast?q=brasil&appid=b94313ed55c5f333932023fa6ebdb431&cnt=56"
-    // ).then((res) => res.json()),
   });
 
   const firstData = data?.list[0];
 
   console.log("data", data);
 
-  if (isPending)
+  if (isLoading)
     return (
       <div className="flex items-center min-h-screen justify-center">
         <p className="animate-bounce">Loading...</p>
@@ -96,8 +99,10 @@ export default function Home() {
         <section className="space-y-4">
           <div className="space-y-4">
             <h2 className="flex gap-1 text-2xl items-end">
-              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
-              <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})</p>
+            <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+            <p className="text-lg">
+                    ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
+            </p>
             </h2>
             <Container className="gap-10 px-6 items-center">
               <div className="flex flex-col px-4">
@@ -130,6 +135,8 @@ export default function Home() {
                     className="flex flex-col judtify-between gap-2 items-center text0-xs font-semibold"
                   >
                     <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), "h:mm a")}</p>
+
+                    <WeatherIcon iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)} />
                     <p className="flex">
                       {convertKelvinToCelsius(d.main.temp ?? 0)}
                       <span className="text-1xl">°C</span>
@@ -139,9 +146,21 @@ export default function Home() {
               </div>
             </Container>
           </div>
+          <div className="flex gap-4">
+            <Container className="w-fit justify-ceneter flex-col px-4 items-center">
+              <p className="capitalize text-center">{firstData?.weather[0].description}</p>
+              <WeatherIcon iconName={getDayOrNightIcon(firstData?.weather[0].icon ?? "", firstData?.dt_txt ?? "")} />
+            </Container>
+            <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto">
+              <WeatherDetails visibility={metersToKilometers(firstData?.visibility ?? 10000)} airPressure={`${firstData?.main.pressure}hPa`} humidity={`${firstData?.main.humidity}%`} sunrise={format(fromUnixTime(data?.city.sunrise ?? 1702949452), "H:mm")} sunset={format(fromUnixTime(data?.city.sunset ?? 1702949452), "H:mm")} windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)} />
+            </Container>
+          </div>
         </section>
 
-        <section></section>
+        {/* 7 day forecast data */}
+        <section className="flex w-full flex-col gap-4">
+          <p className="text-2xl">Forecast (7 days)</p>
+        </section>
       </main>
     </div>
   );
--- src/components/WeatherIcon.tsx
+++ src/components/WeatherIcon.tsx
@@ -4,15 +4,15 @@ import { cn } from "@/utils/cn";
 
 type Props = {};
 
-export default function WeatherIcon({}: Props) {
+export default function WeatherIcon(props: React.HTMLProps<HTMLDivElement> & {iconName: string}) {
   return (
-    <div className={cn("relative h-20 w-20")}>
+    <div {...props} className={cn("relative h-20 w-20")}>
       <Image
         width={100}
         height={100}
         alt="weather icon"
         className="absolute h-full w-full"
-        src={`https://openweathermap.org/img/wn/${iconName}@4x.png`}
+        src={`https://openweathermap.org/img/wn/${props.iconName}@4x.png`}
       />
     </div>
   );
