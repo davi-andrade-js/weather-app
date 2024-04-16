0f1d95d688229a1d8efefc23b18b274c69d914f0
Davi <dv.andrade04@gmail.com>
2024-04-16 15:32:52 -0300

    add forecast section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,108 +1,126 @@
-"use client";
+'use client'
 
-
-import Container from "@/components/Container";
-import Navbar from "@/components/Navbar";
-import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
-import { useQuery } from "react-query";
-import axios from "axios";
-import { format, fromUnixTime, parseISO } from "date-fns";
-import WeatherIcon from "@/components/WeatherIcon";
-import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
-import WeatherDetails from "@/components/WeatherDetails";
-import { metersToKilometers } from "@/utils/metersToKilometers";
-import { convertWindSpeed } from "@/utils/convertWindSpeed";
+import Container from '@/components/Container'
+import Navbar from '@/components/Navbar'
+import ForecastWeatherDetail from '@/components/ForecastWeatherDetail'
+import WeatherDetails from '@/components/WeatherDetails'
+import WeatherIcon from '@/components/WeatherIcon'
+import { convertKelvinToCelsius } from '@/utils/convertKelvinToCelsius'
+import { getDayOrNightIcon } from '@/utils/getDayOrNightIcon'
+import { metersToKilometers } from '@/utils/metersToKilometers'
+import { convertWindSpeed } from '@/utils/convertWindSpeed'
+import { format, fromUnixTime, parseISO } from 'date-fns'
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { placeAtom } from './atom'
+import { useAtom } from 'jotai'
+import { useEffect } from 'react'
 
 interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
+  id: number
+  main: string
+  description: string
+  icon: string
 }
 
 interface Main {
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  sea_level: number;
-  grnd_level: number;
-  humidity: number;
-  temp_kf: number;
+  temp: number
+  feels_like: number
+  temp_min: number
+  temp_max: number
+  pressure: number
+  sea_level: number
+  grnd_level: number
+  humidity: number
+  temp_kf: number
 }
 
 interface Wind {
-  speed: number;
-  deg: number;
-  gust?: number;
+  speed: number
+  deg: number
+  gust?: number
 }
 
 interface WeatherData {
-  dt: number;
-  main: Main;
-  weather: Weather[];
-  clouds: { all: number };
-  wind: Wind;
-  visibility: number;
-  pop: number;
-  rain?: { "3h": number };
-  sys: { pod: "d" | "n" };
-  dt_txt: string;
+  dt: number
+  main: Main
+  weather: Weather[]
+  clouds: { all: number }
+  wind: Wind
+  visibility: number
+  pop: number
+  rain?: { '3h': number }
+  sys: { pod: 'd' | 'n' }
+  dt_txt: string
 }
 
 interface City {
-  id: number;
-  name: string;
-  coord: { lat: number; lon: number };
-  country: string;
-  population: number;
-  timezone: number;
-  sunrise: number;
-  sunset: number;
+  id: number
+  name: string
+  coord: { lat: number; lon: number }
+  country: string
+  population: number
+  timezone: number
+  sunrise: number
+  sunset: number
 }
 
 type WeatherResponse = {
-  cod: number;
-  message: number;
-  cnt: number;
-  list: WeatherData[];
-  city: City;
-};
+  cod: number
+  message: number
+  cnt: number
+  list: WeatherData[]
+  city: City
+}
 
 export default function Home() {
-  const {isLoading, error, data } = useQuery<WeatherResponse>({
-    queryKey: ["repoData"],
+  const [place, setPlace] = useAtom(placeAtom)
+
+  const { isLoading, error, data, refetch } = useQuery<WeatherResponse>({
+    queryKey: ['repoData'],
     queryFn: async () => {
       const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
-      );
-      return data;
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      )
+      return data
     },
-  });
+  })
 
-  const firstData = data?.list[0];
+  useEffect(() => {
+    refetch()
+  }, [place, refetch])
 
-  console.log("data", data);
+  const firstData = data?.list[0]
+
+  console.log('data', data)
+
+  const uniqueDates = [...new Set(data?.list.map((entry) => new Date(entry.dt * 1000).toISOString().split('T')[0]))]
+
+  // Filtering data to get the first entry after 6 AM for each unique date
+  const firstDataForEachDate = uniqueDates.map((date) => {
+    return data?.list.find((entry) => {
+      const entryDate = new Date(entry.dt * 1000).toISOString().split('T')[0]
+      const entryTime = new Date(entry.dt * 1000).getHours()
+      return entryDate === date && entryTime >= 6
+    })
+  })
 
   if (isLoading)
     return (
       <div className="flex items-center min-h-screen justify-center">
         <p className="animate-bounce">Loading...</p>
       </div>
-    );
+    )
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar />
+      <Navbar location={data?.city.name} />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
         <section className="space-y-4">
           <div className="space-y-4">
             <h2 className="flex gap-1 text-2xl items-end">
-            <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
-            <p className="text-lg">
-                    ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
-            </p>
+              <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
+              <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ''), 'dd.MM.yyyy')})</p>
             </h2>
             <Container className="gap-10 px-6 items-center">
               <div className="flex flex-col px-4">
@@ -130,11 +148,8 @@ export default function Home() {
               </div>
               <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3">
                 {data?.list.map((d, i) => (
-                  <div
-                    key={i}
-                    className="flex flex-col judtify-between gap-2 items-center text0-xs font-semibold"
-                  >
-                    <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), "h:mm a")}</p>
+                  <div key={i} className="flex flex-col judtify-between gap-2 items-center text0-xs font-semibold">
+                    <p className="whitespace-nowrap">{format(parseISO(d.dt_txt), 'h:mm a')}</p>
 
                     <WeatherIcon iconName={getDayOrNightIcon(d.weather[0].icon, d.dt_txt)} />
                     <p className="flex">
@@ -149,10 +164,17 @@ export default function Home() {
           <div className="flex gap-4">
             <Container className="w-fit justify-ceneter flex-col px-4 items-center">
               <p className="capitalize text-center">{firstData?.weather[0].description}</p>
-              <WeatherIcon iconName={getDayOrNightIcon(firstData?.weather[0].icon ?? "", firstData?.dt_txt ?? "")} />
+              <WeatherIcon iconName={getDayOrNightIcon(firstData?.weather[0].icon ?? '', firstData?.dt_txt ?? '')} />
             </Container>
             <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto">
-              <WeatherDetails visibility={metersToKilometers(firstData?.visibility ?? 10000)} airPressure={`${firstData?.main.pressure}hPa`} humidity={`${firstData?.main.humidity}%`} sunrise={format(fromUnixTime(data?.city.sunrise ?? 1702949452), "H:mm")} sunset={format(fromUnixTime(data?.city.sunset ?? 1702949452), "H:mm")} windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)} />
+              <WeatherDetails
+                visibility={metersToKilometers(firstData?.visibility ?? 10000)}
+                airPressure={`${firstData?.main.pressure}hPa`}
+                humidity={`${firstData?.main.humidity}%`}
+                sunrise={format(fromUnixTime(data?.city.sunrise ?? 1702949452), 'H:mm')}
+                sunset={format(fromUnixTime(data?.city.sunset ?? 1702949452), 'H:mm')}
+                windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)}
+              />
             </Container>
           </div>
         </section>
@@ -160,8 +182,27 @@ export default function Home() {
         {/* 7 day forecast data */}
         <section className="flex w-full flex-col gap-4">
           <p className="text-2xl">Forecast (7 days)</p>
+          {firstDataForEachDate.map((d, i) => (
+            <ForecastWeatherDetail
+              key={i}
+              description={d?.weather[0].description ?? ''}
+              weatherIcon={d?.weather[0].icon ?? '01d'}
+              date={d ? format(parseISO(d.dt_txt), 'dd.MM') : ''}
+              day={d ? format(parseISO(d.dt_txt), 'dd.MM') : 'EEEE'}
+              feels_like={d?.main.feels_like ?? 0}
+              temp={d?.main.temp ?? 0}
+              temp_max={d?.main.temp_max ?? 0}
+              temp_min={d?.main.temp_min ?? 0}
+              airPressure={`${d?.main.pressure} hPa `}
+              humidity={`${d?.main.humidity}% `}
+              sunrise={format(fromUnixTime(data?.city.sunrise ?? 1702517657), 'H:mm')}
+              sunset={format(fromUnixTime(data?.city.sunset ?? 1702517657), 'H:mm')}
+              visibility={`${metersToKilometers(d?.visibility ?? 10000)} `}
+              windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)} `}
+            />
+          ))}
         </section>
       </main>
     </div>
-  );
+  )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,61 @@
-import React from "react";
-import { FaLocationCrosshairs, FaLocationDot } from "react-icons/fa6";
-import { IoIosSunny } from "react-icons/io";
-import SearchBox from "./SearchBox";
+'use client'
 
-type Props = {};
+import React, { useState } from 'react'
+import { FaLocationCrosshairs, FaLocationDot } from 'react-icons/fa6'
+import { IoIosSunny } from 'react-icons/io'
+import SearchBox from './SearchBox'
+import axios from 'axios'
+import { placeAtom } from '@/app/atom'
+import { useAtom } from 'jotai'
 
-export default function Navbar({}: Props) {
+type Props = { location?: string }
+
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY
+
+export default function Navbar({ location }: Props) {
+  const [city, setCity] = useState('')
+  const [error, setError] = useState('')
+
+  const [suggestions, setSuggestions] = useState<string[]>([])
+  const [showSuggestions, setShowSuggestions] = useState(false)
+  const [place, setPlace] = useAtom(placeAtom)
+
+  async function handleInputChange(value: string) {
+    setCity(value)
+    if (value.length >= 3) {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${API_KEY}&cnt=56`
+        )
+
+        const suggestions = response.data.list.map((item: any) => item.name)
+        setSuggestions(suggestions)
+        setError('')
+        setShowSuggestions(true)
+      } catch (error) {
+        setSuggestions([])
+        setShowSuggestions(false)
+      }
+    } else {
+    }
+  }
+
+  function handleSuggestionClick(value: string) {
+    setCity(value)
+    setShowSuggestions(false)
+  }
+
+  function handleSubmitSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    if (suggestions.length === 0) {
+      setError('Location not found')
+    } else {
+      setError('')
+      setPlace(city)
+      setShowSuggestions(false)
+    }
+  }
+
   return (
     <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
       <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
@@ -16,12 +66,43 @@ export default function Navbar({}: Props) {
         <section className="flex gap-2 items-center">
           <FaLocationCrosshairs className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
           <FaLocationDot className="text-2xl" />
-          <p className="text-slate-900/80 text-sm">Brasil</p>
-          <div>
-            <SearchBox value={""} onChange={undefined} onSubmit={undefined} />
+          <p className="text-slate-900/80 text-sm">{location}</p>
+          <div className="relative">
+            <SearchBox value={city} onChange={(e) => handleInputChange(e.target.value)} onSubmit={handleSubmitSearch} />
+            <SuggestionBox {...{ showSuggestions, suggestions, handleSuggestionClick, error }} />
           </div>
         </section>
       </div>
     </nav>
-  );
+  )
+}
+
+function SuggestionBox({
+  showSuggestions,
+  suggestions,
+  handleSuggestionClick,
+  error,
+}: {
+  showSuggestions: boolean
+  suggestions: string[]
+  handleSuggestionClick: (suggestion: string) => void
+  error: string
+}) {
+  return (
+    <>
+      {((showSuggestions && suggestions.length > 1) || error) && (
+        <ul className="mb-4 bg-white absolute border top-[44px] left-0 border-gray300 rounded-md min-w-[200px] flex flex-col gap-1 py-2 px-4">
+          {error && suggestions.length < 1 && <li className="text-red-500">{error}</li>}
+          {suggestions.map((item, i) => (
+            <li
+              key={i}
+              onClick={() => handleSuggestionClick(item)}
+              className="cursor-pointer p-1 rounded hover:bg-gray-200"
+            ></li>
+          ))}
+          <li className="cursor-pointer p-1 rounded hover:bg-gray-200"></li>
+        </ul>
+      )}
+    </>
+  )
 }
