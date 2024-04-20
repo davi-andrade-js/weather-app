848ef30c0918ffeffe5db5a70280ce85f1ca5290
Davi <dv.andrade04@gmail.com>
2024-04-20 11:37:11 -0300

    update header name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -85,7 +85,7 @@ export default function Navbar({ location }: Props) {
       <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
         <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
           <div className="flex items-center justify-center gap-2">
-            <h2 className="text-gray-500 text-3xl">Weather</h2>
+            <h2 className="text-gray-500 text-3xl">Weather Whisperer</h2>
             <IoIosSunny className="text-3xl mt-1 text-yellow-300" />
           </div>
           <section className="flex gap-2 items-center">
